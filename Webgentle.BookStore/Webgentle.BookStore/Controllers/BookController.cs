a8d0833bb84a1814957e74092ba0cf2a9586dd7c
Sampat Singla <P10496135@capita.co.uk>
2020-12-27 12:36:30 +0000

    Book Changes

---
+++ Webgentle.BookStore/Webgentle.BookStore/Controllers/BookController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Webgentle.BookStore.Models;
+using Webgentle.BookStore.Repository;
+
+namespace Webgentle.BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookRepository = null;
+        public BookController()
+        {
+            _bookRepository = new BookRepository();
+        }
+
+        public ViewResult GetAllBooks()
+        {
+            return View(_bookRepository.GetAllBooks());
+
+        }
+
+        public BookModel GetBook(int id)
+        {
+            return _bookRepository.GetBookbyId(id);
+        }
+
+        public List<BookModel> Searchbook(string bookname, string authorname)
+        {
+            return _bookRepository.SearchBook(bookname, authorname);
+        }
+    }
+
+}
---
+++ Webgentle.BookStore/Webgentle.BookStore/Repository/BookRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Webgentle.BookStore.Models;
+
+namespace Webgentle.BookStore.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return bookModels();
+        }
+
+        public BookModel GetBookbyId(int id)
+        {
+            return bookModels().Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public List<BookModel> SearchBook(string title, string author)
+        {
+            return bookModels().Where(x => x.Title.Contains(title) || x.Author.Contains(author)).ToList();
+        }
+       
+
+        private List<BookModel> bookModels()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel() { Id = 1, Author= "Sampat Singla" , Title ="MVC"},
+                new BookModel() { Id =2, Author = "Sampat" , Title = "Core MVC"},
+                new BookModel() { Id =2, Author = "Singla" , Title = "ASP.NET"}
+            };
+        }
+    }
+}
