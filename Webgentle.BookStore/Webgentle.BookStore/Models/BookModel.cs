d208a2c1c5c3f2c377bed9d1a7a018bf698eb4d8
Sampat Singla <P10496135@capita.co.uk>
2020-12-28 12:14:42 +0000

    GetAllBooks view change

--- Webgentle.BookStore/Webgentle.BookStore/Models/BookModel.cs
+++ Webgentle.BookStore/Webgentle.BookStore/Models/BookModel.cs
@@ -12,5 +12,7 @@ namespace Webgentle.BookStore.Models
         public string Title { get; set; }
 
         public string Author { get; set; }
+
+        public string Description { get; set; }
     }
 }
